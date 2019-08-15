2f88b468925014d7582c1150ed08042fef0d7b33
AndrewSRea <andrew.s.rea@gmail.com>
2019-08-14 17:18:52 -0700

    Replacing deleted files

---
+++ src/images.js
@@ -0,0 +1,23 @@
+import stan from ".images/stan.jpg";
+import kyle from ".images/kyle.jpg";
+import kenny from ".images/kenny.jpg";
+import cartman from ".images/cartman.jpg";
+import butters from ".images/butters.jpg";
+import jimmy from ".images/jimmy.jpg";
+import timmy from ".images/timmy.jpg";
+import token from ".images/token.jpg";
+import tweek from ".images/tweek.jpg";
+
+const images = [
+    stan,
+    kyle,
+    kenny,
+    cartman,
+    butters,
+    jimmy,
+    timmy,
+    token,
+    tweek
+];
+
+export default images;
